b6f4591982e3c61e5722ccf2e4ed9bf6b1f68c9b
Chris Ellis <ellis.cs@gmail.com>
2020-10-13 10:23:28 -0400

    Fetch blog post and display on site

---
+++ pages/api/extractBlogPost.js
@@ -0,0 +1,35 @@
+const extractor = require('unfluff')
+
+export default function handler(req, res) {
+  const blogPost = {
+    url: "https://www.jamesqquick.com/blog/top-5-pieces-of-advice-for-aspiring-and-learning-developers"
+  }
+
+  fetch(blogPost.url).then(async function (response) {
+    // Fetch text from site
+    const res = await response.text();
+    return res;
+  }).then(async function (data) {
+    // This is the JSON from our response
+    const extractedData = await extractor(data)
+    console.log(extractedData)
+
+    // Send response
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(extractedData))
+  }).catch(function (err) {
+    // There was an error
+    console.warn('Something went wrong.', err);
+
+    res.statusCode = 418
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({
+      IMA: 'teapot'
+    }))
+  });
+
+
+
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
 import Nav from '../components/nav'
+import { useState, useEffect } from "react";
 
-export default function IndexPage() {
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+
+  const res = await fetch('http://localhost:3000/api/extractBlogPost')
+  const posts = await res.json()
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+export default function IndexPage({ posts }) {
+
+  const [article, setArticle] = useState({})
+
+  useEffect(() => {
+    console.log(posts)
+    posts.text = posts.text.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    setArticle(posts)
+  }, [posts])
+
+  function createMarkup() { return { __html: article.text }; };
+
   return (
     <div>
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
+          {article.title}
         </h1>
+        <div className="mt-8 w-3/5 mx-auto border-gray-700 border-dashed border-2 p-4" dangerouslySetInnerHTML={createMarkup()}>
+        </div>
       </div>
     </div>
   )
